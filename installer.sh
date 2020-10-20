9b3414d2554ade7351fd3e715eed0ed1748eb459
danielrotta <danielrotta13@gmail.com>
2020-10-20 14:43:39 -0500

    installs

---
+++ installer.sh
@@ -0,0 +1,9 @@
+# update
+sudo apt update
+sudo apt upgrade
+
+# basic packages
+sudo apt install curl git xz-utils openjdk-8-jdk git-extras hwinfo
+
+# snap pack installs
+sudo snap install android-studio flutter code discord steam --classic
