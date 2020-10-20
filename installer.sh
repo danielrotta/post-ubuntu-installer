ac62d72bf4aee2423ae176ca478000fa8a90f7a5
danielrotta <danielrotta13@gmail.com>
2020-10-20 15:02:44 -0500

    separated snap packs

--- installer.sh
+++ installer.sh
@@ -1,15 +1,19 @@
 # update
-sudo apt update
-sudo apt upgrade
+sudo apt update;
+sudo apt upgrade;
 
 # basic packages
-sudo apt install curl git xz-utils openjdk-8-jdk git-extras hwinfo
+sudo apt install curl git xz-utils openjdk-8-jdk git-extras hwinfo;
 
 # snap pack installs
-sudo snap install android-studio flutter code discord steam --classic
+sudo snap install android-studio --classic;
+sudo snap install flutter --classic;
+sudo snap install code --classic;
+sudo snap install discord --classic;
+sudo snap install steam --classic;
 
 # environments setup
-export JAVA_HOME=/usr/bin/java
+export JAVA_HOME=/usr/bin/java;
 
 # for each repo:
 # git config credential.helper store
