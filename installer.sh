bcab7ba268e02faf8c096189c69595f40dbc2a7f
danielrotta <danielrotta13@gmail.com>
2020-10-20 15:06:36 -0500

    removed steam

--- installer.sh
+++ installer.sh
@@ -10,7 +10,6 @@ sudo snap install android-studio --classic;
 sudo snap install flutter --classic;
 sudo snap install code --classic;
 sudo snap install discord --classic;
-sudo snap install steam --classic;
 
 # environments setup
 export JAVA_HOME=/usr/bin/java;
