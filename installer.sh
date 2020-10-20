ee1d543d92fd8ea9fd61454a41cbda56f975c982
danielrotta <danielrotta13@gmail.com>
2020-10-20 15:41:51 -0500

    changes yeet

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@ sudo apt update;
 sudo apt upgrade;
 
 # basic packages
-sudo apt install curl git xz-utils openjdk-8-jdk git-extras hwinfo gimp kdenlive -y;
+sudo apt install curl git xz-utils openjdk-8-jdk git-extras hwinfo -y;
 
 # snap pack installs
 sudo snap install android-studio --classic;
@@ -13,6 +13,10 @@ sudo snap install flutter --classic;
 sudo snap install code --classic;
 sudo snap install discord;
 sudo snap install zoom-client;
+sudo snap install spotify;
+sudo snap install gimp;
+sudo snap install kdenlive;
+sudo snap install vlc;
 
 # environments setup
 export JAVA_HOME=/usr/bin/java;
