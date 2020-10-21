e27cde9d6057ae860d217ca3c9dd5daa68a12418
danielrotta <danielrotta13@gmail.com>
2020-10-21 14:26:41 -0500

    transparency now set to 30

--- installer.sh
+++ installer.sh
@@ -30,7 +30,7 @@ gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Naut
 # set terminal to transparent
 dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-theme-transparency false
 dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-transparent-background true
-dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/background-transparency-percent 40
+dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/background-transparency-percent 30
 
 # for each repo:
 # git config credential.helper store
