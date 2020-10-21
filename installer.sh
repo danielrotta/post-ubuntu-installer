4316b500325f9da956240a40c2a131666e7c9ade
danielrotta <danielrotta13@gmail.com>
2020-10-20 21:05:07 -0500

    background and favorites

--- installer.sh
+++ installer.sh
@@ -8,20 +8,25 @@ sudo apt upgrade;
 sudo apt install curl git xz-utils git-extras hwinfo openjdk-8-jdk -y;
 
 # snap pack installs
-sudo snap install android-studio --classic;
-sudo snap install flutter --classic;
-sudo snap install code --classic;
-sudo snap install discord;
-sudo snap install zoom-client;
-sudo snap install spotify;
-sudo snap install gimp;
-sudo snap install kdenlive;
-sudo snap install vlc;
+sudo snap install android-studio --classic -y;
+sudo snap install flutter --classic -y;
+sudo snap install code --classic -y;
+sudo snap install discord -y;
+sudo snap install zoom-client -y;
+sudo snap install spotify -y;
+sudo snap install gimp -y;
+sudo snap install kdenlive -y;
+sudo snap install vlc -y;
 
 # environment setup
 export JAVA_HOME=/usr/bin/java;
 
 # appearances
+# set background
+gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/hardy_wallpaper_uhd.png;
+
+# set favorites
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'snap-store_ubuntu-software.desktop', 'code_code.desktop', 'discord_discord.desktop']";
 
 # git downloads and config setup
 
